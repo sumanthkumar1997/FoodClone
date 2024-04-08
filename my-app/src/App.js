f8996bba03d3b43d93e8c7dfdda83ddf26aaae10
sumanthkumar1997 <41052225+sumanthkumar1997@users.noreply.github.com>
2024-04-08 18:46:36 +0530

    Header added.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,18 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and sasve to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  
+        <img className="Logo" src={logo}/>
+
+        <ul className='nav'>
+           <li>Home</li>
+           <li>About Us</li>
+           <li>More</li>
+        </ul>
+        
       </header>
     </div>
   );
