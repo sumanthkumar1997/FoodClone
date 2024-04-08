2c61816c0fc238814f1a872a80bd50a5b635f4f3
sumanthkumar1997 <41052225+sumanthkumar1997@users.noreply.github.com>
2024-04-08 18:49:29 +0530

    Header as a functional component.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+  return (
+<header className="App-header">
+  <img className="Logo" src={logo}/>
+
+  <ul className='nav'>
+     <li>Home</li>
+     <li>About Us</li>
+     <li>More</li>
+  </ul>
+  
+</header>
+  );
+} 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-  
-        <img className="Logo" src={logo}/>
-
-        <ul className='nav'>
-           <li>Home</li>
-           <li>About Us</li>
-           <li>More</li>
-        </ul>
-        
-      </header>
+      <Header/>
     </div>
   );
 }
