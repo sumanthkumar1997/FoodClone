b62a773bbb0e2849ec786f5990b26396b656d953
sumanthkumar1997 <41052225+sumanthkumar1997@users.noreply.github.com>
2024-04-09 00:13:13 +0530

    Card added.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,10 +16,47 @@ const Header = () => {
   );
 } 
 
+const Card = () => {
+  return (
+      <div className='cardContainer'>
+        <img className='res-img' alt='res-logo' src='https://images.ctfassets.net/cnu0m8re1exe/2h7AqPkPuB6vk0Gcb1a8DM/6d77bc6ffad48ab44348ece23140ebb6/unhealthy_foods.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill'/>
+        <div className='res-name'>
+          <h3>Hotel Name</h3>
+          <h4>⭐️4.5</h4>
+        </div>
+        <h4>Idli Vada Sambar</h4>
+        <h4>15min</h4>
+      </div>
+  );
+}
+
+const BodyComponent = () => {
+  return (
+    <div className='bodyContainer'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <Header/>
+      <BodyComponent/>
+      
     </div>
   );
 }
